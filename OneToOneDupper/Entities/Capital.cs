c88705a3ccc0e9ed87477b0af4e859fb1f898849
Mahs_kz07 <Mahs_kz07@STHQ011A-16>
2021-10-29 15:48:41 +0400

    Add project files.

---
+++ OneToOneDupper/Entities/Capital.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneToOneDupper.Entities
+{
+    public class Capital
+    {
+        public int CapitalId { get; set; }
+        public string Name { get; set; }
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+    }
+}
---
+++ OneToOneDupper/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using OneToOneDupper.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OneToOneDupper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (var connection=new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString))
+            {
+                var sql = @"select Capitals.CapitalId,Capitals.Name,Capitals.CountryId
+                                       ,Countries.CountryId,Countries.Name 
+                                        from Capitals
+                                         inner join Countries
+                                         on Capitals.CountryId=Countries.CountryId";
+                var capitals = connection.Query<Capital, Country, Capital>(sql,
+                    (capital, country) =>
+                    {
+                        capital.Country = country;
+                        return capital;
+                    },splitOn: "CountryId").ToList();
+
+                MyDataGrid.ItemsSource = capitals;
+               
+            }
+        }
+    }
+}
